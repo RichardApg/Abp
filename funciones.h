c11404e8e1a0ed3549f04880ba17b7902d012f69
RichardApg <158624513+RichardApg@users.noreply.github.com>
2024-11-22 08:38:27 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,11 +1,12 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-void ingresarProductos(char nombres[][50], int tiempos[], int recursos[], int cantidades[], int *numProductos);
-void editarProducto(char nombres[][50], int tiempos[], int recursos[], int cantidades[], int numProductos);
-void eliminarProducto(char nombres[][50], int tiempos[], int recursos[], int cantidades[], int *numProductos);
-void calcularTotales(int tiempos[], int recursos[], int cantidades[], int numProductos, int *tiempoTotal, int *recursosTotal);
-int verificarFactibilidad(int tiempoTotal, int recursosTotal, int tiempoDisponible, int recursosDisponibles);
-void mostrarProductos(char nombres[][50], int tiempos[], int recursos[], int cantidades[], int numProductos);
+float leerDecimalPositivo(const char* mensaje);
+void ingresarProductos(char nombres[][50], float tiempos[], float recursos[], float cantidades[], int *numProductos);
+void editarProducto(char nombres[][50], float tiempos[], float recursos[], float cantidades[], int numProductos);
+void eliminarProducto(char nombres[][50], float tiempos[], float recursos[], float cantidades[], int *numProductos);
+void calcularTotales(float tiempos[], float recursos[], float cantidades[], int numProductos, float *tiempoTotal, float *recursosTotal);
+int verificarFactibilidad(float tiempoTotal, float recursosTotal, float tiempoDisponible, float recursosDisponibles);
+void mostrarProductos(char nombres[][50], float tiempos[], float recursos[], float cantidades[], int numProductos);
 
 #endif
